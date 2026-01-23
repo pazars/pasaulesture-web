98ebda60c69a91e1bdafb881d0df244102a08f22
davispazars <davis.pazars@gmail.com>
2026-01-23 13:12:21 +0200

    #11 Add Google Tag Manager

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -5,6 +5,7 @@ import LocaleProvider from "@/app/components/LocaleProvider";
 import { Geist, Geist_Mono } from "next/font/google";
 import * as m from "@/paraglide/messages";
 import type { Metadata } from "next";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -64,9 +65,28 @@ export default async function LocaleLayout({
 
   return (
     <html lang={locale}>
+      <head>
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WR2D5CTT');`}
+        </Script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WR2D5CTT"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <LocaleProvider locale={locale as Locale}>{children}</LocaleProvider>
       </body>
     </html>
